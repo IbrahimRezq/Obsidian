3488aed34db042ff947cd8cc0337fc55d47ef21a
Ibrahim-Rezq <ovic391@gmail.com>
2023-10-06 14:48:20 +0300

    Quartz sync: Oct 6, 2023, 1:48 PM

---
+++ content/Untitled.md
@@ -0,0 +1,12 @@
+>[!quote]
+>hello
+>help me
+>
+
+
+```ad-quote
+asdg
+df
+df
+
+```
--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Ibrahim_Youssef",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
