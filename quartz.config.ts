87ca7bf9d56055a1a6be8b8fa635b8fad2a43382
Ibrahim-Rezq <ovic391@gmail.com>
2023-12-31 14:35:37 +0200

    initial setup

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Ibrahim_Youssef",
+    pageTitle: "المدرسة الربانية",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -14,15 +14,8 @@ export default ((opts?: Options) => {
       <footer class={`${displayClass ?? ""}`}>
         <hr />
         <p>
-          Created with <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a>, © {year}
+          {/* Created with <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a>, © {year} */}
         </p>
-        <ul>
-          {Object.entries(links).map(([text, link]) => (
-            <li>
-              <a href={link}>{text}</a>
-            </li>
-          ))}
-        </ul>
       </footer>
     )
   }
